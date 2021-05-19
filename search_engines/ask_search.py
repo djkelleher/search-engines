e81d9276dd2a7d4bdce0f68a3f19d041d78fbd8c
dan <kelleherjdan@gmail.com>
2021-05-19 12:13:33 -0400

    update next page path

--- search_engines/ask_search.py
+++ search_engines/ask_search.py
@@ -17,7 +17,7 @@ def extract_search_results(html: str, page_url: str) -> Tuple[List[Dict[str, str
             'page_number': page_number or "1",
         } for result in root.xpath('//div[@class="PartialSearchResults-item"]')]
     next_page_url = extract_first(
-        root.xpath('//li[@class="PartialWebPagination-next"]/parent::a/@href'))
+        root.xpath('//li[@class="PartialWebPagination-next"]/a/@href'))
     if next_page_url:
         next_page_url = 'https://www.ask.com' + next_page_url
     return results, next_page_url
